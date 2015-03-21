73c6bc9bb608940dca946f9a83ce4b63149fabe7
G Bartz <gerald.bartz@t-online.de>
2015-03-21 18:11:16 +0100

    commit of progassignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a class of enhanced matrices which are able to cache its inverses.
 
-## Write a short comment describing this function
+## Creates environment which represents an enhanced matrix class
+## Is able to hold its inverse in cache so it has not to be recomputed
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        print("inv")
+        if (exists("inv")){print(inv)}
+        inv <- NULL
+        set <- function(y) {
+                m <<- y
+                inv <<- NULL
+        }
+        get <- function() m
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'm'
+## Writes inverses into environment of input CacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        ## Return a matrix that is the inverse of 'm'
+        inv <- m$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- m$get()
+        inv <- solve(data, ...)
+        m$setinverse(inv)
+        inv
 }
